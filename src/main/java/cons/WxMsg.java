eb0f7f59d8088557aa1ca6bfe1803134e463a9ad
scorego <javior@qq.com>
2019-06-20 20:27:35 +0800

    update

--- src/main/java/cons/WxMsg.java
+++ src/main/java/cons/WxMsg.java
@@ -8,12 +8,17 @@ package cons;
  */
 public class WxMsg {
     /**
-     *  微信消息换行符
+     * 微信消息换行符
      */
     public static final String LINE = "\n";
 
     /**
-     * 微信群里@人后面的类似空格的字符。不是空格。如“@nickname ” \u2005?
+     * 微信群里@人后面的类似空格的字符。不是空格。如“@nickname ”  \u2005 ?？
      */
     public static final String AT_ME_SPACE = " ";
+
+    /**
+     *  接收的消息内容的换行。
+     */
+    public static final String RECEIVE_MSG_LINE = "<br/>";
 }
