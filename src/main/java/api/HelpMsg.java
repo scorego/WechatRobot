dcb4ad25d7f6b995e45c0520b008169a9df0caf8
scorego <javior@qq.com>
2019-06-21 13:53:01 +0800

    update

--- src/main/java/api/HelpMsg.java
+++ src/main/java/api/HelpMsg.java
@@ -17,11 +17,12 @@ public class HelpMsg {
     public static String getHelpMsg() {
         String commandPrefix = PreProcessMessage.getCommandPrefix();
         String helpMsg = "WeChat指令聊天机器人" + WxMsg.LINE
-                + "指令前缀：" + commandPrefix + WxMsg.LINE
+                + "【指令前缀】" + commandPrefix + WxMsg.LINE
                 + "指令前缀+具体指令开启相应模式。" + WxMsg.LINE
-                + "天气模式：" + "输入城市查询天气，如北京天气。" + WxMsg.LINE
-                + "更多模式：" + "开发中" + WxMsg.LINE
-                + "试着发一下\"" + PreProcessMessage.getCommandPrefix() +"天气\"。" + WxMsg.LINE
+                + "【天气模式】" + "输入城市查询天气，如北京天气。" + WxMsg.LINE
+                + "【极速模式】" + "查天气快捷指令：" +  "？" + WxMsg.LINE
+                + "【更多模式】" + "开发中" + WxMsg.LINE
+                + "试着发一下“" + PreProcessMessage.getCommandPrefix() +"天气”。☺" + WxMsg.LINE
                 + "详情见" + README_LINK + WxMsg.LINE;
 
         return helpMsg;
