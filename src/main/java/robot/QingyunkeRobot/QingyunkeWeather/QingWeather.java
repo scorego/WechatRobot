e15f0d605fff7d6ba6043684c60ad8c831da9763
scorego <javior@qq.com>
2019-07-02 12:43:40 +0800

    update

--- src/main/java/robot/QingyunkeRobot/QingyunkeWeather/QingWeather.java
+++ src/main/java/robot/QingyunkeRobot/QingyunkeWeather/QingWeather.java
@@ -8,14 +8,14 @@ import utils.HttpRequestUtil;
 
 public class QingWeather {
 
-    private static final String QING_WEATHER = GlobalConfig.getValue("QingyunkeRobot.weather","");
+    private static final String QING_WEATHER = GlobalConfig.getValue("QingyunkeRobot.weather", "");
 
     public static String getWeatherByCityId(String cityId) {
         if (StringUtils.isBlank(cityId)) {
             return null;
         }
         String response = HttpRequestUtil.doGet(QING_WEATHER + cityId);
-        if (StringUtils.isBlank(response)){
+        if (StringUtils.isBlank(response)) {
             return null;
         }
         QingWeatherEntity weather = JSON.parseObject(response, QingWeatherEntity.class);
@@ -31,17 +31,19 @@ public class QingWeather {
                 keyWord.substring(2) : keyWord.substring(0, keyWord.length() - 2);
         return QingWeather.getWeatherByCityName(cityName);
     }
+
     public static String getWeatherByCityName(String cityName) {
         if (StringUtils.isBlank(cityName)) {
             return null;
         }
 
         // 部分地区有问题，已知的有：北京朝阳区和辽宁朝阳市；福田区；浦东新区
-        switch (cityName){
+        switch (cityName) {
             case "朝阳区":
             case "朝阳":
                 return QingWeather.getWeatherByCityId("101010300");
             case "福田区":
+            case "福田":
                 return null;
             case "浦东新区":
             case "浦东":
